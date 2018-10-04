17a32d5c1e3570dbc1f35237b1a0c5fac8e03b8c
Isaac Zais <isaaczais@hotmail.com>
2018-10-04 15:37:34 -0500

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,4 @@
+modlist = ['ArtificialBadger#1308']
+whitelist = ['daleshingles#4558', 'DRay313#4722', 'ArtificialBadger#1308']
+app_secret = "NDg5NjE1NDkxMDc4MjkxNDU2.DntX6w.CHJnrwoNLpm971p5kkgBkESifRs"
+test_secret = "asdfasdfasdfasdf"
