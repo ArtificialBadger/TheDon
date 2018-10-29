c3d47da6b87f70c2c14a082c817b1afd4732d145
Isaac Zais <isaaczais@hotmail.com>
2018-10-29 15:57:28 -0500

    Added enum for CancelOptions

--- Config.py
+++ Config.py
@@ -1,7 +1,17 @@
+from enum import Enum
+
+class CancelOptions(Enum):
+    EVERYONE = 1
+    NONE = 2
+    MODS = 3
+    WHITELIST = 4
+
 modlist = []
 whitelist = []
 app_secret = ""
 currency = "RA Bucks"
 currency_code = "RAB"
 starting_amount = 100
-allow_cancels = False
+allow_cancels = CancelOptions.MODS
+allow_purges = False
+
