83c11519fee5280a1fcb9bfdb9adf00453aadf68
Isaac Zais <isaaczais@hotmail.com>
2019-03-17 19:50:56 -0500

    Autolocking date functions

--- functions.py
+++ functions.py
@@ -1,7 +1,18 @@
-from datetime import datetime
 from pytz import timezone
+from datetime import datetime, time
+from dateutil import tz, parser
 
 def to_time(utc_time):
     ratime = timezone('America/Chicago')
     localtime = utc_time.astimezone(ratime)
-    return localtime.strftime('%b %d %H:%M')1
+    return localtime.strftime('%b %d %H:%M')
+
+def from_time(ra_time):
+    #ratime = timezone('America/Chicago')
+    utctimezone = timezone('Etc/Greenwich')
+    utctime = ra_time.astimezone(utctimezone)
+    return utctime.strftime('%b %d %H:%M')
+
+def my_parser(*args, default_tzinfo=tz.gettz("America/Chicago"), **kwargs):
+    dt = parser.parse(*args, **kwargs)
+    return dt.replace(tzinfo=dt.tzinfo or default_tzinfo)
