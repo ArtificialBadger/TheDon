7c491efb74c8facef1a4ffb22ca0965a8ee78ba2
Isaac Zais <isaaczais@hotmail.com>
2019-04-02 13:02:53 -0500

    Add Random Team

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 from pytz import timezone
-from datetime import datetime, time
+from datetime import date, datetime, time
 from dateutil import tz, parser
+import random
 
 def to_time(utc_time):
     ratime = timezone('America/Chicago')
@@ -16,3 +17,13 @@ def from_time(ra_time):
 def my_parser(*args, default_tzinfo=tz.gettz("America/Chicago"), **kwargs):
     dt = parser.parse(*args, **kwargs)
     return dt.replace(tzinfo=dt.tzinfo or default_tzinfo)
+
+
+def random_team():
+    random.seed(str(date.today()))
+
+    a = ["Atlanta Braves","Miami Marlins","New York Mets","Philadelphia Phillies","Washington Nationals","Chicago Cubs","Cincinnati Reds","Milwaukee Brewers","Pittsburgh Pirates","St.Louis Cardinals","Arizona Diamondbacks","Colorado Rockies","Los Angeles Dodgers","San Diego Padres","San Francisco Giants","Baltimore Orioles","Boston Red Sox","New York Yankees","Tampa Bay Rays","Toronto Blue Jays","Chicago White Sox","Cleveland Indians","Detroit Tigers","Kansas City Royals","Minnesota Twins","Houston Astros","Los Angeles Angels","Oakland Athletics","Seattle Mariners","Texas Rangers"]
+
+    random.shuffle(a)
+
+    return a[0]
