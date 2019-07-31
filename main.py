d6936819a8a79c3c7715dc544c403130c99d9a16
Isaac Zais <isaaczais@hotmail.com>
2019-07-30 22:43:51 -0700

    2nd mimic

--- main.py
+++ main.py
@@ -83,19 +83,43 @@ async def mimic(ctx):
     async for message in ctx.history(limit=5000, oldest_first=False):
         if str(message.author) == str(ctx.message.author) and not message.content == "" and not message.author.bot:
             if (not "$" in message.content):
-                megastring += "\r\n" + message.content
+                megastring += ". " + message.content
                 count += 1
-    if len(megastring) <10:
+    if len(megastring) < 10:
         await ctx.send("No can do buckaroo Count: " + str(count) + megastring)
     try:
         chain = markovify.Text(megastring)
-        sentence = chain.make_short_sentence(max_chars=100, max_overlap_ratio=.8)
+        sentence = chain.make_short_sentence(max_chars=64, max_overlap_ratio=.95)
         #await ctx.send("Count: " + str(count))
         if sentence is None:
             raise Exception('Fuck')
         else:
             await ctx.send(sentence)
     except:
+        #await ctx.send(megastring)
+        await ctx.send("No can do buckaroo")
+
+@bot.command(pass_context=True)
+async def mimic2(ctx):
+    megastring = ""
+    count = 0
+    async for message in ctx.history(limit=5000, oldest_first=False):
+        if str(message.author) == str(ctx.message.author) and not message.content == "" and not message.author.bot:
+            if (not "$" in message.content):
+                megastring += "\r\n" + message.content
+                count += 1
+    if len(megastring) < 10:
+        await ctx.send("No can do buckaroo Count: " + str(count) + megastring)
+    try:
+        chain = markovify.Text(megastring)
+        sentence = chain.make_short_sentence(max_chars=100, max_overlap_ratio=.95)
+        #await ctx.send("Count: " + str(count))
+        if sentence is None:
+            raise Exception('Fuck')
+        else:
+            await ctx.send(sentence)
+    except:
+        #await ctx.send(megastring)
         await ctx.send("No can do buckaroo")
 
 @bot.command(pass_context=True)
