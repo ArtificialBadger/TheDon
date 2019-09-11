8cbd81868e0e95ca9045a2eeb1ece86b843c8e8c
Isaac Zais <isaaczais@hotmail.com>
2019-09-11 14:20:50 -0700

    Added edit, allow user

--- mimic.py
+++ mimic.py
@@ -15,27 +15,33 @@ class Mimic(commands.Cog):
         await ctx.send('Hello from the Mimic Cog')
 
     @commands.command(brief="", description="")
-    async def mimic(self, ctx):
+    async def mimic(self, ctx, user: discord.Member = None):
+        mimicMessage = await ctx.send("Working on it...")
+
         megastring = ""
         count = 0
+
+        if user is None:
+            mimicUser = str(ctx.message.author)
+        else:
+            mimicUser = str(user)
+
         async for message in ctx.history(limit=5000, oldest_first=False):
-            if str(message.author) == str(ctx.message.author) and not message.content == "" and not message.author.bot:
+            if str(message.author) == mimicUser and not message.content == "" and not message.author.bot:
                 if not "$" in message.content:
-                    megastring += ". " + message.content
+                    megastring += "\r\n" + message.content
                     count += 1
         if len(megastring) < 10:
             await ctx.send("No can do buckaroo Count: " + str(count) + megastring)
         try:
-            chain = markovify.Text(megastring)
-            sentence = chain.make_short_sentence(max_chars=64, max_overlap_ratio=.95)
-            # await ctx.send("Count: " + str(count))
+            chain = markovify.NewlineText(megastring)
+            sentence = chain.make_short_sentence(max_chars=100, max_overlap_ratio=.95)
             if sentence is None:
                 raise Exception('Fuck')
             else:
-                await ctx.send(sentence)
+                await mimicMessage.edit(content=sentence)
         except:
-            # await ctx.send(megastring)
-            await ctx.send("No can do buckaroo")
+            await ctx.send("You don't talk enough in here :angry:")
 
     @commands.command(brief="", description="")
     async def mimic2(self, ctx):
